78c866970e697b417517eae2a4763b6c74395810
vinay pandey <vinaypandey0801gmail.com>
2024-06-03 05:07:04 -0700

    Doing Network calls and andling response the right way

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
@@ -5,7 +6,24 @@ import { useSelector } from 'react-redux';
 
 function App() {
   const show = useSelector((store)=>store.ui.cartIsVisible)
-  console.log(show);
+  const cart = useSelector((store)=>store.cart)
+  console.log(cart);
+  useEffect(()=>{
+fetch('https://expensetracker-7f8dd-default-rtdb.firebaseio.com/cart.json',
+  {
+    method:'PUT',
+    body: JSON.stringify(cart),
+  }).then((req)=>{
+    if(req.ok)
+      {
+        console.log("ok");
+      }
+      else{
+        console.log('error');
+      }
+  })
+  },[cart])
+
   return (
     <Layout>
     {show && (<Cart />)}  
